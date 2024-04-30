9ee01fb565d0383dbb786a44ac2fad135749f39b
JasirV <jasirmon133@gmail.com>
2024-04-30 11:33:44 +0530

    api

--- Client/src/components/VerificationCheck.jsx
+++ Client/src/components/VerificationCheck.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const VerificationCheck = () => {
+  const navigation=useNavigate()
   setInterval(()=>{
     navigation("/login")
   },5000)
