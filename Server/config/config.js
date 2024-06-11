b2037385770ac8d0055cc4a7af94da501481788e
JasirV <jasirmon133@gmail.com>
2024-06-11 12:13:47 +0530

    Update

--- Server/config/config.js
+++ Server/config/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   await mongoose
-    .connect("mongodb+srv://fixeca4451:x9UkVvB3aZznXZYW@cluster0.laedogl.mongodb.net/")
+    .connect("mongodb+srv://jasirmon133:3W6A9aoUY2yDio2a@cluster0.sbrhdax.mongodb.net/")
     .then((conn) => {
       console.log("Connection success");
     })
